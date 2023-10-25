77d70da91ceb3460e036513b208de08b2f6346c9
Marta Organisciak <marta.organisciak@wp.pl>
2023-10-25 15:20:10 +0200

    pages, routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Homepage from "./Pages/Homepage";
-import Gamepage from "./Pages/Gamepage";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import GamePage from "./Pages/GamePage";
+import CartPage from "./Pages/CartPage";
+import WishlistPage from "./Pages/WishlistPage";
+import AuthPage from "./Pages/AuthPage";
+import ProfilePage from "./Pages/ProfilePage";
 
 function App() {
   return (
     <>
-      {/* <Router> */}
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/game" element={<Gamepage />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/cart" element={<ShoppingCart />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/user" element={<UserProfile />} />
-        <Route />
-      </Routes>
-      {/* </Router> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/game" element={<GamePage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/user" element={<ProfilePage />} />
+          <Route />
+        </Routes>
+      </Router>
     </>
   );
 }
