ec755ae5d3f1a6805e7c631724692a9ab5401ad4
Marta Organisciak <marta.organisciak@wp.pl>
2023-10-26 13:35:05 +0200

    footer tailwind parat

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
       <h1>HELLO</h1>
+      <Footer />
     </>
   );
 }
