4a192f0309f2bbe90597aaf400f2d981738fc165
Piotr Groblewski <groblewskip@gmail.com>
2023-10-26 18:17:32 +0200

    categories

---
+++ hooks/getCategories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const getCategories = () => {
+  return (
+    <div>getCategories</div>
+  )
+}
+
+export default getCategories;
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar.jsx";
+import Categories from "../components/Categories.jsx";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
       <h1>HELLO</h1>
+      <Categories />
     </>
   );
 }
