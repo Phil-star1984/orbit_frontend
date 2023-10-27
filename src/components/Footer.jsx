7194122199d8e5ff5db953724fa4983e0846f416
Marta Organisciak <marta.organisciak@wp.pl>
2023-10-27 09:25:11 +0200

    footer className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,38 +3,40 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <div class="w-full min-h-screen flex items-center justify-center bg-black">
-        <div class="md:w-2/3 w-full px-4 text-white flex flex-col">
-          <div class="w-full text-7xl font-bold">
-            <h1 class="w-full md:w-2/3">How can we help you? Get in touch.</h1>
+      <div className="w-full min-h-screen flex items-center justify-center bg-black">
+        <div className="md:w-2/3 w-full px-4 text-white flex flex-col">
+          <div className="w-full text-7xl font-bold">
+            <h1 className="w-full md:w-2/3">
+              How can we help you? Get in touch.
+            </h1>
           </div>
-          <div class="flex mt-8 flex-col md:flex-row md:justify-between">
-            <p class="w-full md:w-2/3 text-gray-400">
+          <div className="flex mt-8 flex-col md:flex-row md:justify-between">
+            <p className="w-full md:w-2/3 text-gray-400">
               Our goal is to offer you the best user experiences with your
               games. We would like to know if you are happy with our store.
             </p>
-            <div class="w-44 pt-6 md:pt-0">
-              <a class="bg-red-500 justify-center text-center rounded-lg shadow px-10 py-3 flex items-center">
+            <div className="w-44 pt-6 md:pt-0">
+              <a className="bg-red-500 justify-center text-center rounded-lg shadow px-10 py-3 flex items-center">
                 Contact US
               </a>
             </div>
           </div>
-          <div class="flex flex-col">
-            <div class="flex mt-24 mb-12 flex-row justify-between">
-              <div class=""></div>
-              <a class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
+          <div className="flex flex-col">
+            <div className="flex mt-24 mb-12 flex-row justify-between">
+              <div className=""></div>
+              <a className="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
                 About
               </a>
-              <a class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
+              <a className="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
                 Services
               </a>
-              <a class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
+              <a className="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
                 Why us
               </a>
-              <a class="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
+              <a className="hidden md:block cursor-pointer text-gray-600 hover:text-white uppercase">
                 Contact
               </a>
-              <div class="flex flex-row space-x-8 items-center justify-between">
+              <div className="flex flex-row space-x-8 items-center justify-between">
                 <a>
                   <svg
                     width="6"
@@ -87,8 +89,8 @@ function Footer() {
                 </a>
               </div>
             </div>
-            <hr class="border-gray-600" />
-            <p class="w-full text-center my-12 text-gray-600">
+            <hr className="border-gray-600" />
+            <p className="w-full text-center my-12 text-gray-600">
               Copyright © 2023 WDG#012
             </p>
           </div>
