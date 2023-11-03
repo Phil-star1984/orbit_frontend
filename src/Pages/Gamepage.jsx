576f158737464ab0270019f40122029d53880101
Marta Organisciak <marta.organisciak@wp.pl>
2023-11-03 14:01:53 +0100

    video

--- src/Pages/Gamepage.jsx
+++ src/Pages/Gamepage.jsx
@@ -3,6 +3,7 @@ import useFetchRAWG from "../../hooks/useFetchRAWG.jsx";
 import calcArbitraryPrice from "../../utility/calcArbetraryPrice.jsx";
 import Footer from "../components/Footer.jsx";
 import { useParams } from "react-router-dom";
+import VideosTrailers from "../components/VideosTrailers.jsx";
 
 export default function GamePage() {
   return (
@@ -11,6 +12,7 @@ export default function GamePage() {
       <div>Price Box</div>
       <div>Tag Cloud</div>
       <div>Videos & Trailers </div>
+      <VideosTrailers />
       <div>Ratings & Metacritic</div>
       <div>Genres</div>
       <div>Description</div>
--- src/components/buttons/ExploreBtn.jsx
+++ src/components/buttons/ExploreBtn.jsx
@@ -1,12 +1,21 @@
 import react from "react";
 import { Button } from "@material-tailwind/react";
+import GamePage from "../../Pages/Gamepage";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default function ExploreBtn() {
   <>
     <div>
+      <Link to="/game">
+        <button size="lg" color="white">
+          Explore
+        </button>
+      </Link>
+
+      {/* <Link to="/beispielseite">Beispielseite</Link>
       <Button size="lg" color="white">
         Explore
-      </Button>
+      </Button> */}
     </div>
   </>;
 }
