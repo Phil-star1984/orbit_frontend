b8ffa896cf546a2f8e862c87a65080b01c923fc2
Marta Organisciak <marta.organisciak@wp.pl>
2023-11-05 17:17:35 +0100

    deal

--- src/Pages/DealPagePiotr.jsx
+++ src/Pages/DealPagePiotr.jsx
@@ -1,4 +1,3 @@
-import { CheckIcon } from "@heroicons/react/20/solid";
 import React from "react";
 
 import axios from "axios";
@@ -85,57 +84,17 @@ function DealPage() {
           />
         </div>
         <div>
-          {/* <PriceBox /> */}
-          <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-            <div
-              className="rounded-2xl bg-blue-200 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center
-       lg:py-16"
-            >
-              <div className="mx-auto max-w-xs px-8">
-                <p className="text-base font-semibold text-gray-700">
-                  Pay once, own it forever
-                </p>
-                <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                  <span className="text-4xl font-bold tracking-tight  bg-green-200 text-gray-900">
-                    <p>
-                      {" "}
-                      {-(
-                        (1 - deal.salePrice / deal.normalPrice) *
-                        100
-                      ).toFixed() + "% OFF"}
-                    </p>
-                  </span>
-                  <span className="text-7xl font-bold tracking-tight  bg-yellow-200 text-gray-900">
-                    <p>{deal.salePrice}</p>
-                    <span className="text-sm font-semibold leading-8 tracking-wide text-gray-600">
-                      <p>{deal.normalPrice}</p>
-                    </span>
-                  </span>
-                </p>
-                <a
-                  href="#"
-                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Buy now
-                </a>
-                <a
-                  href="#"
-                  className="mt-10 block w-full rounded-md bg-yellow-800 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Add to Cart
-                </a>
-                <p className="mt-6 text-xs leading-5 text-gray-600">
-                  Invoices and receipts available for easy company reimbursement
-                </p>
-              </div>
-            </div>
-          </div>
+          <PriceBox 
+          <p>{deal.salePrice}</p>
+          <p>{deal.normalPrice}</p>
+          />
         </div>
         ESRB Rating:
         {detailsGameData.esrb_rating === null
           ? " n/a"
           : detailsGameData.esrb_rating.name}
         <div>
+          Price Box
           <p>{deal.salePrice}</p>
           <p>{deal.normalPrice}</p>
           <p>
