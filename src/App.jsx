cc0492252901748660fa9c26745e365bc638bdb5
Marta Organisciak <marta.organisciak@wp.pl>
2023-11-06 17:22:35 +0100

    404

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,44 @@
-import { useState } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home.jsx';
-import CartPage from './Pages/CartPage.jsx';
-import WishlistPage from './Pages/WishlistPage.jsx';
-import AuthPage from './Pages/AuthPage.jsx';
-import ProfilePage from './Pages/ProfilePage.jsx';
-import AllGamesPage from './Pages/AllGamesPage.jsx';
-import CategoriesPage from './Pages/CategoriesPage.jsx';
-import DealPage from './Pages/DealPage.jsx';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
-import ShowResults from './components/ShowResults.jsx';
-import SignUp from './components/SignUp.jsx';
-import GamePage from './Pages/Gamepage.jsx';
-import 'react-toastify/dist/ReactToastify.css';
-import ProductPage from './Pages/ProductPage.jsx';
-
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import CartPage from "./Pages/CartPage.jsx";
+import WishlistPage from "./Pages/WishlistPage.jsx";
+import AuthPage from "./Pages/AuthPage.jsx";
+import ProfilePage from "./Pages/ProfilePage.jsx";
+import AllGamesPage from "./Pages/AllGamesPage.jsx";
+import CategoriesPage from "./Pages/CategoriesPage.jsx";
+import DealPage from "./Pages/DealPage.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import ShowResults from "./components/ShowResults.jsx";
+import SignUp from "./components/SignUp.jsx";
+import GamePage from "./Pages/Gamepage.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import ProductPage from "./Pages/ProductPage.jsx";
+import NotFound from "./components/NotFound.jsx";
 
 function App() {
   const [results, setResults] = useState(null);
 
   return (
-    <div className='bg-gray-900'>
+    <div className="bg-gray-900">
       <ToastContainer />
       <Navbar setResults={setResults} />
       {results && <ShowResults results={results} />}
       <Routes>
-
-        <Route path='/' element={<Home />} />
-        <Route path='/game' element={<GamePage />} />
-        <Route path='/wishlist' element={<WishlistPage />} />
-        <Route path='/cart' element={<CartPage />} />
-        <Route path='/login' element={<AuthPage />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/user' element={<ProfilePage />} />
-        <Route path='/allgames' element={<AllGamesPage />} />
-        <Route path='/categories/:id' element={<CategoriesPage />} />
-        <Route path='/categories/:id/:gameID' element={<ProductPage />} />
-        <Route path='/deals/:rawTitle' element={<DealPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/user" element={<ProfilePage />} />
+        <Route path="/allgames" element={<AllGamesPage />} />
+        <Route path="/categories/:id" element={<CategoriesPage />} />
+        <Route path="/categories/:id/:gameID" element={<ProductPage />} />
+        <Route path="/deals/:rawTitle" element={<DealPage />} />
+        <Route path="*" element={<NotFound />} />
 
         <Route />
       </Routes>
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+// import orbitLogo from "/src/assets/orbitLogo.svg";
+import ExploreBtn from "./buttons/ExploreBtn";
+
+function NotFound() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-900">
+        <div className="text-center">
+          <img
+            className="h-32 w-auto mb-12"
+            src={orbitLogo}
+            alt="Orbit Gaming Logo"
+          />
+          <h1 className="text-9xl font-bold text-white mb-4">404</h1>
+          <p className="text-xl text-white mb-8">Page Not Found</p>
+
+          <Link to="/">
+            <button className="bg-white hover:opacity-70 text-black font-bold py-2 px-4 rounded-md">
+              GO BACK
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NotFound;
