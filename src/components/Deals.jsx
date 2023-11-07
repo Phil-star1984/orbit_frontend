d04977e99873b0a8233b8c8aed343542759e75bb
Piotr Groblewski <groblewskip@gmail.com>
2023-11-07 15:52:45 +0100

    topdealstopdealstopdeals

--- src/components/Deals.jsx
+++ src/components/Deals.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import PacmanLoader from "react-spinners/PacmanLoader";
 import axios from "axios";
+import api from "../../api/apiRAWG.jsx";
 import { useState, useEffect } from "react";
 import AddToCartBtn from "./buttons/AddToCartBtn";import DealsItem from "./DealsItem.jsx";
 
 
 function Deals() {
+  const key = import.meta.env.VITE_KEY;
   const [deals, setDeals] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
 
   useEffect(() => {
     const getDeals = async () => {
@@ -15,9 +19,31 @@ function Deals() {
         const response = await axios.get(
           "https://www.cheapshark.com/api/1.0/deals?storeID=1&onSale=1&pageSize=12"
         );
-
+        let urls = [
+          `/games?&search=${response.data[0].title}&key=${key}`,
+          `/games?&search=${response.data[1].title}&key=${key}`,
+          `/games?&search=${response.data[2].title}&key=${key}`,
+          `/games?&search=${response.data[3].title}&key=${key}`,
+          `/games?&search=${response.data[4].title}&key=${key}`,
+          `/games?&search=${response.data[5].title}&key=${key}`,
+          `/games?&search=${response.data[6].title}&key=${key}`,
+          `/games?&search=${response.data[7].title}&key=${key}`,
+          `/games?&search=${response.data[8].title}&key=${key}`,
+          `/games?&search=${response.data[9].title}&key=${key}`,
+          `/games?&search=${response.data[10].title}&key=${key}`,
+          `/games?&search=${response.data[11].title}&key=${key}`,
+         
+        ];
         setDeals(response.data);
+
+        await Promise.all(urls.map((url) => api.get(url)))
+        .then( (data)=>{
+          setData(data);
+        });
+        
+       
         setLoading(false);
+      
       } catch (error) {
         console.error(error);
       }
@@ -37,7 +63,8 @@ function Deals() {
       </div>
     );
   }
-  //console.log(deals);
+
+
 
   return (
     <div className="deals mx-48 ">
@@ -46,7 +73,7 @@ function Deals() {
       </h3>
       {/* main div - Grid Container */}
       <div className="grid grid-cols-2 m-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mb-16">
-        {deals.map((deal) => (
+        {deals.map((deal, index) => (
           <Link
             key={deal.dealID}
             to={`/deals/${deal.title}`}
@@ -59,7 +86,7 @@ function Deals() {
               <img
                 className="w-full h-full md:h-auto rounded-lg"
                 alt={`${deal.title}`}
-                src={`${deal.thumb}`}
+                src={`${data[index].data.results[0].background_image}`}
               />
               <div className="w-full flex gap-2  justify-end items-center mt-4">
                 <p className="text-xl text-white font-semibold  bg-pink px-4 py-2 rounded-lg text-center">
@@ -74,7 +101,8 @@ function Deals() {
                   <p className="text-white text-lg">{deal.salePrice} €</p>
                 </div>
                 <div>
-                  <AddToCartBtn />
+
+                  <AddToCartBtn gameId={data[index].data.results[0].id}/>
                 </div>
               </div>
             </div>
