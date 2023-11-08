735f1bd7c365558761b33664400e2be88d1ee17b
Philipp Mulfinger <info@phil-splash.de>
2023-11-08 12:02:12 +0100

    Fixed Autoplay

--- src/components/CarouselForDeals.jsx
+++ src/components/CarouselForDeals.jsx
@@ -99,7 +99,7 @@ export default function CarouselForDeals({
           alt="image 3"
           className="h-full w-full object-cover"
         />
-        <div className="absolute inset-0 grid h-full w-full items-end bg-black/75">
+        <div className="absolute inset-0 grid h-full w-full items-end bg-black/25">
           {/* < div className="w-3/4 pl-12 pb-12 md:w-2/4 md:pl-20 md:pb-20 lg:pl-32 lg:pb-32">
             <Typography
               variant="h1"
@@ -136,7 +136,7 @@ export default function CarouselForDeals({
           alt="image 4"
           className="h-full w-full object-cover"
         />
-        <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+        <div className="absolute inset-0 grid h-full w-full items-center bg-black/25">
           {/* <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
             <Typography
               variant="h1"
@@ -173,7 +173,7 @@ export default function CarouselForDeals({
           alt="image 5"
           className="h-full w-full object-cover"
         />
-        <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+        <div className="absolute inset-0 grid h-full w-full items-center bg-black/25">
           {/* <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
             <Typography
               variant="h1"
@@ -211,7 +211,7 @@ export default function CarouselForDeals({
           alt="image 6"
           className="h-full w-full object-cover"
         />
-        <div className="absolute inset-0 grid h-full w-full items-center bg-black/75">
+        <div className="absolute inset-0 grid h-full w-full items-center bg-black/25">
           {/* <div className="w-3/4 pl-12 md:w-2/4 md:pl-20 lg:pl-32">
             <Typography
               variant="h1"
